bb8c781974416f0ee3496ac1ddcff9ecf253fd80
Roidy Alvarez <Roidy.1207@gmail.com>
2021-01-25 19:52:08 -0500

    ver 2

--- src/main/java/com/panweb/model/Productos.java
+++ src/main/java/com/panweb/model/Productos.java
@@ -2,12 +2,15 @@ package com.panweb.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Productos {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@Column(name = "Nombre",length = 50)
